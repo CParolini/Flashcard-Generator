e8565ec580be214cc8b17fbecd67b0b1f1c51feb
Chris Parolini <chris.parolini@chatid.com>
2017-01-30 17:50:14 -0600

    adding functionality to pull questions from nfl.js and ncaa.js

--- flashcard.js
+++ flashcard.js
@@ -3,3 +3,7 @@ console.log("This is flashcard.js!");
 //Required packages
 var inquirer = require("inquirer");
 var fs = require("fs");
+
+//Load question files
+var basicCard = require("./nfl.js");
+var clozeCard = require("./ncaa.js");
