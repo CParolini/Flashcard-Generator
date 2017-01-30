f0b3d9acf7ffb76c92ad35c8c5223b3f7eda01e1
Chris Parolini <chris.parolini@chatid.com>
2017-01-30 17:40:06 -0600

    questions for nfl flash cards

--- nfl.js
+++ nfl.js
@@ -1,1 +1,17 @@
 console.log("These are NFL cards!");
+exports.basicQuestions = [{
+    front: "Who was the coach of the Green Bay Packers who won 4 championship and whose name would eventually be used to name to Super Bowl Trophy?",
+    back: "Vince Lombardi"
+}, {
+    front: "Who is the winningest coach in the NFL regular season?",
+    back: "Don Shula"
+}, {
+    front: "In what year did the AFL and NFL merge?",
+    back: "1970"
+}, {
+    front: "What NFL team has won the most Super Bowls and how many?",
+    back: "Pittsburgh Steelers with 6"
+}, {
+    front: "Who is the oldest living NFL player and how old is he?",
+    back: "Clarence 'Ace' Parker at 105 years old"
+}];
