5d7916e9abfce1249d8ba4c839d5d21b93e1da44
Chris Parolini <chris.parolini@chatid.com>
2017-01-30 17:55:06 -0600

    adding start game function and calling it at the beginning to start when flashcard.js is called

--- flashcard.js
+++ flashcard.js
@@ -10,3 +10,22 @@ var clozeCard = require("./ncaa.js");
 
 var count = 0;
 var score = 0;
+
+startgame();
+
+var startgame = function () {
+  count = 0;
+  score = 0;
+  inquirer.prompt([{
+    type: "list",
+    message: "Choose from the Following:",
+    choices: ["Basic", "Cloze"],
+    name: "chooseType"
+  }]).then(function (answers) {
+    if (answers.chooseType === "Basic") {
+      askQuestion();
+    } else {
+      askCloze();
+    }
+  });
+}
