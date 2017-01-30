e7ab32ff5d59168a2522287ba2e2b31de321521c
Chris Parolini <chris.parolini@chatid.com>
2017-01-30 17:45:56 -0600

    NCAA questions

--- ncaa.js
+++ ncaa.js
@@ -1,1 +1,17 @@
 console.log("These are NCAA cards!");
+exports.clozeQuestions = [{
+    text: "Which college football program has the most wins and how many?",
+    cloze: "Michigan with 915"
+}, {
+    text: "Which team has had the most players drafted in the first round of the NFL?",
+    cloze: "USC"
+}, {
+    text: "Which team has been to the most Bowl Games?",
+    cloze: "Alabama"
+}, {
+    text: "Which team has won the most conference championships?",
+    cloze: "Nebraska"
+}, {
+    text: "Which school has one the most Heisman Trophies?",
+    cloze: "Notre Dame and Ohio State"
+}];
