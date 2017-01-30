07f507fef12bcb75b97899c0be513dd84c94ca76
Chris Parolini <chris.parolini@chatid.com>
2017-01-30 17:59:27 -0600

    function to ask  question

--- flashcard.js
+++ flashcard.js
@@ -13,19 +13,56 @@ var score = 0;
 
 startgame();
 
-var startgame = function () {
-  count = 0;
-  score = 0;
-  inquirer.prompt([{
-    type: "list",
-    message: "Choose from the Following:",
-    choices: ["Basic", "Cloze"],
-    name: "chooseType"
-  }]).then(function (answers) {
-    if (answers.chooseType === "Basic") {
-      askQuestion();
+var askQuestion = function() {
+
+    if (count < basicCard.basicQuestions.length) {
+        inquirer.prompt([{
+            name: "input",
+            message: basicCard.basicQuestions[count].front
+        }]).then(function(answers) {
+            if (answers.input === basicCard.basicQuestions[count].back) {
+                console.log("Keep going!");
+                score++;
+            } else {
+                console.log("Sorry, the correct answer is " + basicCard.basicQuestions[count].back + " Current score = " + score);
+            }
+            count++;
+            askQuestion();
+        });
     } else {
-      askCloze();
+        var gameOver = true;
+        count = 0;
+        if (gameOver === true) {
+            inquirer.prompt([{
+                type: "list",
+                name: "game",
+                message: "Your Score was " + score + " answers correct\n  Would you like to try again?",
+                choices: ["Yes", "No"]
+            }]).then(function(answer) {
+                if (answer.game === "Yes") {
+                    startgame();
+                    endGame = false;
+                } else {
+                    console.log("Until next time.... how bout dem Cowboys@");
+                }
+            });
+        }
     }
-  });
+}
+
+var startgame = function() {
+    count = 0;
+    score = 0;
+    inquirer.prompt([{
+        type: "list",
+        message: "Choose from the Following:",
+        choices: ["Basic", "Cloze"],
+        name: "chooseType"
+    }]).then(function(answers) {
+        if (answers.chooseType === "Basic") {
+            askQuestion();
+        } else {
+            askCloze();
+        }
+    });
 }
